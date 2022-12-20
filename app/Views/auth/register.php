d8e6c4f04344e52ef50c5ab2089fced3dc92c609
RM Abdul Dhohir Surya Kusuma <98220753+rmAbdulDhohir@users.noreply.github.com>
2022-12-20 15:33:52 +0700

    setup register

--- app/Views/auth/register.php
+++ app/Views/auth/register.php
@@ -13,35 +13,39 @@
                         <div class="col-lg">
                             <div class="p-5">
                                 <div class="text-center">
-                                    <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
+                                    <h1 class="h4 text-gray-900 mb-4"><?= lang('Auth.register') ?></h1>
                                 </div>
-                                <form class="user">
+
+                                <?= view('Myth\Auth\Views\_message_block') ?>
+
+                                <form class="user" action="<?= url_to('register') ?>" method="post">
+                                    <?= csrf_field() ?>
 
                                     <div class="form-group">
-                                        <input type="text" class="form-control form-control-user" id="username" placeholder="User Name">
+                                        <input type="text" class="form-control form-control-user <?php if (session('errors.username')) : ?>is-invalid<?php endif ?>" name="username" placeholder="<?= lang('Auth.username') ?>" value="<?= old('username') ?>">
                                     </div>
 
                                     <div class="form-group">
-                                        <input type="email" class="form-control form-control-user" id="email" placeholder="Email Address">
+                                        <input type="email" class="form-control form-control-user <?php if (session('errors.email')) : ?>is-invalid<?php endif ?>" name="email" placeholder="<?= lang('Auth.email') ?>" value="<?= old('email') ?>">
                                     </div>
-
+                                  
                                     <div class="form-group row">
                                         <div class="col-sm-6 mb-3 mb-sm-0">
-                                            <input type="password" class="form-control form-control-user" id="password" placeholder="Password">
+                                            <input type="password" name="password" class="form-control form-control-user <?php if (session('errors.password')) : ?>is-invalid<?php endif ?>" id="password" placeholder="<?= lang('Auth.password') ?>" autocomplete="off">
                                         </div>
                                         <div class="col-sm-6">
-                                            <input type="password" class="form-control form-control-user" id="repeatPassword" placeholder="Repeat Password">
+                                            <input type="password" name="pass_confirm" class="form-control form-control-user <?php if (session('errors.pass_confirm')) : ?>is-invalid<?php endif ?>" id="repeatPassword" placeholder="<?= lang('Auth.repeatPassword') ?>" autocomplete="off">
                                         </div>
                                     </div>
-                                    <a href="<?= base_url(); ?>/index.php" class="btn btn-success btn-user btn-block">
-                                        Register Account
-                                    </a>
+                                    <button type="submit" href="<?= base_url(); ?>/index.php" class="btn btn-success btn-user btn-block">
+                                    <?=lang('Auth.register')?>
+                                    </button>
                                     <hr>
 
                                 </form>
 
                                 <div class="text-center">
-                                    <a class="small" href="<?= base_url(); ?>/index.php">Already have an account? Login!</a>
+                                    <p><a class="small" href="<?= url_to('login') ?>"><?=lang('Auth.alreadyRegistered')?> <?=lang('Auth.signIn')?></a></p>
                                 </div>
                             </div>
                         </div>
